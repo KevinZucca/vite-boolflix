3e3fa7295162b134fc54df17149025627dc1341d
KevinZucca <kev.94@live.it>
2023-04-05 10:57:34 +0200

    fix: fixed missed api in store

--- src/store.js
+++ src/store.js
@@ -59,13 +59,4 @@ export const store = reactive ({
     finalResult: "",
 
 
-    // MOST POPULAR MOVIES
-
-    latestMoviesCall: "https://api.themoviedb.org/3/movie/latest?api_key=4e2a533e46ec71346f9b1fdf830c85b5",
-
-    latestMovieResult: [],
-   
-
-
-
 })
